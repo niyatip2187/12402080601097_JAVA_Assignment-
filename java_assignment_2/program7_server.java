75132f5b5cf16122c9bfd1de95f43f8bb8e6310a
niyatip2187 <niyatip2187@gmail.com>
2026-03-31 17:45:40 +0530

    program7_server.java

---
+++ java_assignment_2/program7_server.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            System.out.println("Server is waiting...");
+
+            Socket socket = server.accept();
+            System.out.println("Client connected!");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            String msg;
+
+            while ((msg = in.readLine()) != null) {
+                System.out.println("Client: " + msg);
+                out.println("Echo: " + msg); // send back
+            }
+
+            socket.close();
+            server.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
